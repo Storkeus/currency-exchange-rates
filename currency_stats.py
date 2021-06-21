a38efe42ed506b88be76d20b539d51048fcdab8a
Bartosz Łyżwa <bartosz.lyzwa@o2.pl>
2021-06-21 23:51:02 +0200

    Refactor mean counting function

--- currency_stats.py
+++ currency_stats.py
@@ -6,16 +6,18 @@ import numpy
 def count_exchange_rates_mean(exchange_rates: numpy.array):
     try:
 
-        if exchange_rates.size == 0:
-            return 0
-
         if numpy.any(exchange_rates < 0):
             raise Exception("Negative values are not allowed")
 
         if exchange_rates.ndim > 1:
             raise Exception("Multidimensional arrays are not allowed")
 
-        return numpy.mean(exchange_rates)
+        if exchange_rates.size == 0:
+            result = 0
+        else:
+            result = numpy.mean(exchange_rates)
+
+        return result
     except:
         return False
 
