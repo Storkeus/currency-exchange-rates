bf254be87c755ea8702c59ce8f5502a287459741
Bartosz Łyżwa <bartosz.lyzwa@o2.pl>
2021-06-21 23:46:31 +0200

    Implement count_mean function

--- currency_stats.py
+++ currency_stats.py
@@ -4,7 +4,20 @@ import numpy
 
 
 def count_exchange_rates_mean(exchange_rates: numpy.array):
-    return
+    try:
+
+        if exchange_rates.size == 0:
+            return 0
+
+        if numpy.any(exchange_rates < 0):
+            raise Exception("Negative values are not allowed")
+
+        if exchange_rates.ndim > 1:
+            raise Exception("Multidimensional arrays are not allowed")
+
+        return numpy.mean(exchange_rates)
+    except:
+        return False
 
 
 def main():
